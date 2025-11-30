55d28873c09f48dbf3b5cb2067f9089a8defa1ad
Yunhan Qiao <yunhanqiao@Yunhans-MacBook-Pro.local>
2025-11-30 10:16:46 -0800

    Update the playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -2,8 +2,14 @@ const { devices } = require("@playwright/test");
 
 const config = {
   outputDir: "./test-results",
+  timeout: 60000,
+  fullyParallel: false,
+  workers: 1,
+  retries: 0,
   use: {
     baseURL: "http://localhost:3000",
+    actionTimeout: 15000,
+    navigationTimeout: 15000,
     // headless: false,
     // launchOptions: {sloMo: 1000,},
   },
